2f6a87b53ecb57dafff3c93104e525c1e699d68e
JISU LEE <wltn102998@gmail.com>
2022-11-02 17:46:05 +0900

    FEAT: 3번 선택된 항목 삭제 기능 완료

--- src/COMPANY.java
+++ src/COMPANY.java
@@ -22,6 +22,10 @@ public class COMPANY {
 	static final Integer bdateRange[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 	static final JComboBox bdateBox = new JComboBox<Integer>(bdateRange);
 	static final JTextField subordinateBox = new JTextField();
+	static final JLabel selectedEmployeeLabel = new JLabel("선택한 직원:");
+	static final JLabel selectedEmployeeCountLabel = new JLabel("인원수:");
+	static final DefaultTableModel currentData = new DefaultTableModel();
+	static final Map<String, String> selectedEmployee = new HashMap<String, String>();
 	
 	
 	static final ArrayList<JCheckBox> searchItemBoxes = new ArrayList<JCheckBox>();
@@ -39,7 +43,7 @@ public class COMPANY {
 		
 		resultTable.setVisible(false);
 		JScrollPane scrollPane = new JScrollPane(resultTable);
-		scrollPane.setBounds(50, 65, 900, 650);
+		scrollPane.setBounds(50, 65, 900, 550);
 		container.add(scrollPane);
 		
 		JPanel panel = new JPanel();
@@ -48,6 +52,7 @@ public class COMPANY {
 		setSearchRanges(panel);
 		setSearchItems(panel);
 		setSearchButton(panel);
+		setResultBar(panel);
 		
 		panel.setVisible(true);
 		container.add(panel);
@@ -73,7 +78,7 @@ public class COMPANY {
 				subordinateBox.setVisible(false);
 				if (s == searchRange[1]) {
 					String sql = "Select dname from DEPARTMENT;";
-					runSQL(sql, (rs) -> {
+					runSelectSQL(sql, (rs) -> {
 						try {
 							DefaultComboBoxModel model = new DefaultComboBoxModel<String>();
 							while (rs.next()) {
@@ -88,7 +93,7 @@ public class COMPANY {
 					});
 				} else if (s == searchRange[2]) {
 					String sql = "Select distinct sex from EMPLOYEE;";
-					runSQL(sql, (rs) -> {
+					runSelectSQL(sql, (rs) -> {
 						try {
 							DefaultComboBoxModel model = new DefaultComboBoxModel<String>();
 							while (rs.next()) {
@@ -190,97 +195,120 @@ public class COMPANY {
 		searchButton.setBounds(750, 35, 70, 25);
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String sql = "Select ";
-				ArrayList<String> column = new ArrayList<String>();
-				column.add("선택");
-				
-				for(JCheckBox item: searchItemBoxes) {
-					if (item.isSelected() == false) {
-						continue;
-					}
-					if (item.getText() == "Name") {
-						sql = sql + "e.Fname as eFname,e.Minit as eMinit,e.Lname as eLname,";
-					} else if (item.getText() == "Supervisor") {
-						sql = sql + "s.Fname as sFname,s.Minit as sMinit,s.Lname as sLname,";
-					} else if (item.getText() == "Department") {
-						sql = sql + "d.Dname as Department,";
-					} else {
-						sql = sql + "e."+item.getText() + ",";
-					}
-					column.add(item.getText());
-				}
-				
-				sql = sql.substring(0, sql.length() - 1) + " from EMPLOYEE e left join EMPLOYEE s ON e.Super_ssn = s.Ssn join DEPARTMENT d ON e.Dno = d.Dnumber ";
-				
-				Integer searchRangeIdx = searchRangeBox.getSelectedIndex();
-				if (searchRangeIdx == 1) {
-					sql = sql + "where d.Dname = '" + (String) departmentBox.getSelectedItem() +"'";
-				} else if (searchRangeIdx == 2) {
-					sql = sql + "where e.Sex = '" + (String) sexBox.getSelectedItem() + "'";
-				} else if (searchRangeIdx == 3) {
-					try {
-						Integer salary = Integer.parseInt(salaryBox.getText());
-						sql = sql + "where e.Salary > " + salary;
-					} catch (NumberFormatException e1) {
-						JOptionPane.showMessageDialog(null, "숫자를 입력해주세요");
-					}
-				} else if (searchRangeIdx == 4) {
-					Integer bmonth = (Integer) bdateBox.getSelectedItem();
-					sql = sql + "where MONTH(e.bdate) = " + bmonth;
-				} else if (searchRangeIdx == 5) {
-					String[] name = ((String) subordinateBox.getText()).split(" ");
-					if (name.length == 3) {
-						String fname = name[0];
-						String minit = name[1];
-						String lname = name[2];
-						sql = sql + "where s.fname = '" + fname + "' AND s.minit = '" + minit + "' AND s.lname = '" + lname + "'";
-					} else {
-						JOptionPane.showMessageDialog(null, "이름을 정상적으로 입력해주세요\n(ex Jennifer S Wallace");
-					}
-				}
-				runSQL(sql+";", rs -> showResult(column, rs));
+				search();
 			}
 		});
 		panel.add(searchButton);
 	}
 	
+	public static void setResultBar(JPanel panel) {
+		selectedEmployeeLabel.setBounds(7, 650, 800, 30);
+		selectedEmployeeLabel.setVisible(true);
+		panel.add(selectedEmployeeLabel);
+		
+		selectedEmployeeCountLabel.setBounds(7, 680, 100, 30);
+		selectedEmployeeCountLabel.setVisible(true);
+		panel.add(selectedEmployeeCountLabel);
+		
+		JButton deleteButton = new JButton("선택한 데이터 삭제");
+		deleteButton.setBounds(800, 680, 150, 30);
+		deleteButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sql = "Delete from EMPLOYEE where ssn IN (";
+				for (String strKey: selectedEmployee.keySet()) {
+					sql = sql + "'" + strKey + "',";
+				} 
+				sql = sql.substring(0, sql.length()-1) + ");";
+				runSQL(sql, (rs) -> {
+					selectedEmployee.clear();
+					selectedEmployeeLabel.setText("선택한 직원: ");
+					search();
+					return null;
+				});
+			}
+		});
+		
+		
+		panel.add(deleteButton);
+	}
+	
 	public static Void showResult(ArrayList<String> column, ResultSet result) {
 		try {
-			DefaultTableModel model = new DefaultTableModel(column.toArray(), 0);
+			DefaultTableModel model = new DefaultTableModel(column.toArray(), 0) {
+				@Override
+				public void setValueAt(Object value, int row, int col) {
+					super.setValueAt(value, row, col);
+				    if (col == 0) {
+				    	String ssn = (String) currentData.getValueAt(row, currentData.getColumnCount()-2);
+				    	String name = (String) currentData.getValueAt(row, currentData.getColumnCount()-1);
+				    	if ((Boolean) value) {
+				    		selectedEmployee.put(ssn, name);
+				    	} else {
+				    		selectedEmployee.remove(ssn);
+				    	}
+				    	String str = "선택한 직원: ";
+				    	for( String strKey : selectedEmployee.keySet() ){
+				    		String val = selectedEmployee.get(strKey);
+				    		str = str + val + ", ";
+				    	}
+				    	selectedEmployeeLabel.setText(str.substring(0, str.length()-2));
+				    }
+				}
+			};
+			column.add("pk");
+			column.add("pkName");
+			currentData.setColumnIdentifiers(column.toArray());
+			currentData.setRowCount(0);
 			while (result.next()) {
-				Object[] data = new Object[column.size()];
+				Object[] data = new Object[column.size() - 2];
+				Object[] pkData = new Object[column.size()];
 				data[0] = false;
-				for(int i = 1; i < column.size(); i++) {
+				String fname = result.getString("eFname");
+				String minit = result.getString("eMinit");
+				String lname = result.getString("eLname");
+				String name = fname+" "+minit+" "+lname;
+				for(int i = 1; i < column.size()-2; i++) {
 					if (column.get(i) == "Name") {
-						String fname = result.getString("eFname");
-						String minit = result.getString("eMinit");
-						String lname = result.getString("eLname");
-						data[i] = fname+" "+minit+" "+lname;
+						data[i] = name;
 					} else if (column.get(i) == "Supervisor") {
 						if (result.getString("sFname") == null) {
 							data[i] = "";
 						} else {
-							String fname = result.getString("sFname");
-							String minit = result.getString("sMinit");
-							String lname = result.getString("sLname");
-							data[i] = fname+" "+minit+" "+lname;
+							String sfname = result.getString("sFname");
+							String sminit = result.getString("sMinit");
+							String slname = result.getString("sLname");
+							data[i] = sfname+" "+sminit+" "+slname;
 						}
 					} else {
 						data[i] = result.getString(column.get(i));				
 					}
+					pkData[i] = data[i];
 				}
+				pkData[column.size()-2] = result.getString("Ssn");
+				pkData[column.size()-1] = name;
 				model.addRow(data);
+				currentData.addRow(pkData);
 			}
 			resultTable.setModel(model);
-			resultTable.getColumnModel().getColumn(0).setCellRenderer(new CellCheckRenderer());
+			
+			resultTable.getColumnModel().getColumn(0).setCellRenderer(new DefaultTableCellRenderer() {
+				@Override
+				public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected,boolean hasFocus, int row,int column){
+					JCheckBox check = new JCheckBox();
+					check.setSelected((Boolean) value);
+					return check;
+				}
+			});
+			resultTable.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(new JCheckBox()));
 			resultTable.setVisible(true);
+			selectedEmployeeCountLabel.setText("인원수: "+model.getRowCount());
 		} catch (SQLException e) {
 			System.out.println("showResult error " + e.getLocalizedMessage());
 		}
 		return null;
 	}
 	
-	public static void runSQL(String sql, Function <ResultSet, Void> completion) {
+	public static void runSelectSQL(String sql, Function <ResultSet, Void> completion) {
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			System.out.println("정상적으로 연결되었습니다");
 			try (Statement stmt = conn.createStatement()) {
@@ -293,23 +321,71 @@ public class COMPANY {
 			System.out.println("runSQL error " + e.getLocalizedMessage());
 		}
 	}
-}
-
-
-class CellCheckRenderer extends DefaultTableCellRenderer{
 	
-	@Override
-	public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected,boolean hasFocus, int row,int column){
-		JCheckBox check = new JCheckBox(); 
-		check.setHorizontalAlignment(SwingConstants.CENTER);
-		return check;
+	public static void runSQL(String sql, Function <Void, Void> completion) {
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			System.out.println("정상적으로 연결되었습니다");
+			try (Statement stmt = conn.prepareStatement(sql)) {
+				System.out.println(sql);
+				int rs = stmt.executeUpdate(sql);
+				if (rs == 1) {
+					completion.apply(null);
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("runSQL error " + e.getLocalizedMessage());
+		}
+	}
+	
+	public static void search() {
+		String sql = "Select e.Ssn, e.Fname as eFname,e.Minit as eMinit,e.Lname as eLname,";
+		ArrayList<String> column = new ArrayList<String>();
+		column.add("선택");
+		
+		for(JCheckBox item: searchItemBoxes) {
+			if (item.isSelected() == false) {
+				continue;
+			} else if (item.getText() == "Ssn" || item.getText() == "Name") {
+				column.add(item.getText());
+				continue;
+			} else if (item.getText() == "Supervisor") {
+				sql = sql + "s.Fname as sFname,s.Minit as sMinit,s.Lname as sLname,";
+			} else if (item.getText() == "Department") {
+				sql = sql + "d.Dname as Department,";
+			} else {
+				sql = sql + "e."+item.getText() + ",";
+			}
+			column.add(item.getText());
+		}
+		
+		sql = sql.substring(0, sql.length() - 1) + " from EMPLOYEE e left join EMPLOYEE s ON e.Super_ssn = s.Ssn join DEPARTMENT d ON e.Dno = d.Dnumber ";
+		
+		Integer searchRangeIdx = searchRangeBox.getSelectedIndex();
+		if (searchRangeIdx == 1) {
+			sql = sql + "where d.Dname = '" + (String) departmentBox.getSelectedItem() +"'";
+		} else if (searchRangeIdx == 2) {
+			sql = sql + "where e.Sex = '" + (String) sexBox.getSelectedItem() + "'";
+		} else if (searchRangeIdx == 3) {
+			try {
+				Integer salary = Integer.parseInt(salaryBox.getText());
+				sql = sql + "where e.Salary > " + salary;
+			} catch (NumberFormatException e1) {
+				JOptionPane.showMessageDialog(null, "숫자를 입력해주세요");
+			}
+		} else if (searchRangeIdx == 4) {
+			Integer bmonth = (Integer) bdateBox.getSelectedItem();
+			sql = sql + "where MONTH(e.bdate) = " + bmonth;
+		} else if (searchRangeIdx == 5) {
+			String[] name = ((String) subordinateBox.getText()).split(" ");
+			if (name.length == 3) {
+				String fname = name[0];
+				String minit = name[1];
+				String lname = name[2];
+				sql = sql + "where s.fname = '" + fname + "' AND s.minit = '" + minit + "' AND s.lname = '" + lname + "'";
+			} else {
+				JOptionPane.showMessageDialog(null, "이름을 정상적으로 입력해주세요\n(ex Jennifer S Wallace");
+			}
+		}
+		runSelectSQL(sql+";", rs -> showResult(column, rs));
 	}
 }
-
-
-
-//while (rs.next()) {
-//	fname = rs.getString(1);
-//	salary = rs.getDouble("Salary");
-//	System.out.printf("Fname: %s Salary: %f\n", fname, salary);
-//}
